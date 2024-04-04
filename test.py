ef956d7da02f0eeb058b5e84611c049414e93457
Ye Wang <38368593+wildkevin@users.noreply.github.com>
2024-04-04 10:11:47 +0800

    Update test.py

--- test.py
+++ test.py
@@ -13,3 +13,4 @@ headers = {
 }
 
 df = pd.DataFrame(columns=['company', 'stitt_output', 'api_return', 'response_time'])
+https://urldefense.com/v3/__https://9hdppg0c.r.ap-southeast-2.awstrack.me/L0/https:*2F*2Fdocs.google.com*2Fdocument*2Fd*2Fe*2F2PACX-1vQ0yDBnu3pX60DW9yTftomGI_bvU4cLbF5bOnkM7LcD1thwlC14t5WHs58h80Gji6kbvpYjTV6oTGSp*2Fpub/1/0108018ea3c9ba05-cd46237d-9274-4fcf-ad37-5d2916522448-000000/4liNcBAcsR63fu6He21NOkazUgY=149__;JSUlJSUlJQ!!LSAcJDlP!3FmkF7K0RzO-IdxmwUDq2ZIMZe7hFQQEW0pW7vk_7fB5YjuxtI4C1RUPZ9Yq1K8Ob6x4kqsKXYMhf--q8faew_x8TMIr9BJtOjF6YsQ$
